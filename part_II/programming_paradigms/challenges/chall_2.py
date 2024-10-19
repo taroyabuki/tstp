fb36709e2cc0d27cd1bcef9b845981bd18c8a047
ybktr <taro.yabuki@it-chiba.ac.jp>
2024-10-19 10:55:19 +0900

    Match the method name to the textbook.
    The name of the method to calculate the area is ``area'' in the textbook. It is not ``calculate_area.''

--- part_II/programming_paradigms/challenges/chall_2.py
+++ part_II/programming_paradigms/challenges/chall_2.py
@@ -5,8 +5,8 @@ class Circle():
     def __init__(self, radius):
         self.radius = radius
 
-    def calculate_area(self):
+    def area(self):
         return self.radius ** 2 * math.pi
 
 a_circle = Circle(4)
-print(a_circle.calculate_area())
+print(a_circle.area())
--- part_II/programming_paradigms/challenges/chall_3.py
+++ part_II/programming_paradigms/challenges/chall_3.py
@@ -3,8 +3,8 @@ class Triangle():
         self.base = base
         self.height = height
 
-    def calculate_area(self):
+    def area(self):
         return self.height * self.base / 2
 
 a_triangle = Triangle(20, 30)
-print(a_triangle.calculate_area())
+print(a_triangle.area())
